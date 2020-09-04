354c5a454e926662cf600b583f6e859271d6ad47
BOX-SNIR <box.thomas@lyceeppr.fr>
2020-09-04 16:56:40 +0200

    Exercice 6

---
+++ exercice6.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int nombre;
+	cout<<"Entrée un nombre\n";
+	cin>>nombre;
+	if(nombre>0){
+		cout<<"Le nombre entrée est positif\n";
+	}else{
+		cout<<"Le nombre entrée est négatif"<<endl;
+	}
+}
